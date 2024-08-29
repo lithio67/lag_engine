1c43b7efadb10f8e4f86035f964f1ab40c4e8233
lithio67 <shliu0607@gmail.com>
2024-08-29 18:37:31 -0500

    lag_engine.py

--- lag_engine.py
+++ lag_engine.py
@@ -4,11 +4,11 @@ import time
 def heavy_computation(process_id):
   while True:
     # Intensive calculations
-    for i in range(1000000):
-      for j in range(1000000):
-        x = i * j * i*i*j
-    print(f"Process {process_id} finished iteration")
-    time.sleep(0.1)
+    for i in range(99999999):
+      for j in range(999999999):
+        x = i * j * i*i*j*i
+    print("Process {process_id} finished iteration")
+    time.sleep(0.001)
 
 if __name__ == '__main__':
   processes = []
